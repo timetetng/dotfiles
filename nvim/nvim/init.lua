eb5dd9f9665420461fad10d0e50098fe5ee33e9a
timetetng <timetetng@gmail.com>
2026-03-07 12:08:49 +0800

    nvim:剪贴板同步

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -5,3 +5,15 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
